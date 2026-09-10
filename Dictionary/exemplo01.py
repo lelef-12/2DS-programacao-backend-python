5f314d2da7428529e67e95d623627566df91e8da
lelef-12 <livia.silva02@escola.pr.gov.br>
2026-09-10 10:29:02 -0300

    Add files via upload

---
+++ Dictionary/exemplo01.py
@@ -0,0 +1,27 @@
+#Lista
+aluno["Ana", 8.5 2]
+print(Aluno[1])
+
+
+#Dicionário
+aluno = {
+    "nome": "ana",
+    "nota": 8.5,
+    "faltas":2
+}
+
+carro = {
+    "marca": "for",
+    "modelo": "mustang",
+    "ano": 1964
+}
+
+jogo = {
+    "titulo": "python quest",
+    "nivel":3,
+    "online": true
+}
+print(jogo["titulo"])
+print(jogo["nivel"])
+pirnt(carro["marca"])
+print(aluno["nome"])
---
+++ Dictionary/exemplo02.py
@@ -0,0 +1,7 @@
+aluno = {"nome": "ana", 'nota':9.0}
+
+if "nota" in aluno:
+    print("nota caastrada")
+
+for chave, vlor in aluno.items():
+    print(chave, "-->", valor)
